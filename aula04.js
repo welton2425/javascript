1a85ba9626a9380a12cc3758624a2abff9a20532
welton2425 <welton.ma.tu@gmail.com>
2025-05-08 21:38:34 -0300

    javascript
    javascript

---
+++ aula04.js
@@ -0,0 +1,15 @@
+//veja baixo exemplo de como e quando usar as crases 
+var nome = 'welton';// criamos uma variavel tipo string para guarda o nome do usuario
+var idade = 31; // criamos uma variavel tipo number par guarda a idade do usuario
+
+var mensagem = ` Olá, meu nome é ${nome} e tenho ${idade} anos`;
+// criamos uma variavel chamada menssagem para exibir o texto contendo as duas variaveis que foram declarada 
+
+console.log(mensagem);
+// criamos o consolo.log para fazer a impressão da variavel "mensagem"
+
+
+var profissao = 'vigilante';
+var salario = '5.500';
+var retornoSalario = ` A profissão dele é ${profissao} e ele recebe ao mes ${salario} reais`;
+console.log(retornoSalario);
---
+++ aula05.js
@@ -0,0 +1,6 @@
+let primeiroNome = 'welton';
+let utimoNome = 'oliveira';
+let nomeCompleto = primeiroNome + ' ' + utimoNome; //usando o + 
+let nomeCompletoTemplate = `${primeiroNome} ${utimoNome};` //using template literal
+
+console.log(nomeCompletoTemplate)
