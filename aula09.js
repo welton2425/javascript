d672229eb8f10a6320bd9e783ca5598f3dd69d7c
welton2425 <welton.ma.tu@gmail.com>
2025-05-16 21:37:24 -0300

    javascript
    javascript

--- aula09.js
+++ aula09.js
@@ -1,5 +1,4 @@
 // tipo de um objeto em javascript
-
 const carta = {
     nome: "pokemon",
     habilidade: 95,
@@ -7,14 +6,28 @@ const carta = {
     ataque: 89,
     slogan: "pokemon amarelo rsrs"
 };
-console.log (`meu personagem si chama: ${carta.nome} a habilidade de é ${carta.habilidade} a defesa dele é  ${carta.defesa} e o ataque dele é ${carta.ataque} mesmo assim meu ${carta.slogan}`);
+
+
+//console.log (`meu personagem si chama ${carta.nome} a habilidade de é ${carta.habilidade} a defesa dele é  ${carta.defesa} e o ataque dele é ${carta.ataque} mesmo assim meu ${carta.slogan}`);
 
 
 const jogo = {
-    nome: "goku",
-    habilidade2; 1.000,
+    nome2: "goku",
+    habilidade2: 1.000,
     defesa2: 800,
     ataque2: 900,
     slogan2: "goku ganho a batalha hehehehe"
 };
-console.log (``)
+//console.log (`${jogo.nome2} participou do toneio de luta com a habilidade de ${jogo.habilidade2}, defesa de ${jogo.defesa2} e ataque de ${jogo.ataque2} e como sempre ${jogo.slogan2}`);
+
+
+const poder = {
+    nome3: "naruto",
+    habilidade3: 2000,
+    defesa3: 1500,
+    ataque3: 1300,
+    slogan3: "raposa de 9 caldas"
+}
+// console.log (`${poder.nome3} esta mais forte com habilidade de ${poder.habilidade3}, defesa de  ${poder.defesa3} e ataque de ${poder.ataque3} com essas habilidade ele consegue virar ${poder.slogan3}`);
+
+
