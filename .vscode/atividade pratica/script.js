a19ae5684a02f0fc3267b7c3cc7ccc0e9c9b719f
welton2425 <welton.ma.tu@gmail.com>
2025-05-20 21:41:24 -0300

    javascript
    javascript

---
+++ .vscode/atividade pratica/script.js
@@ -0,0 +1,26 @@
+//getattiibute - Obter o valor do atributo (valor da linha)
+
+//setAttribute - definir ou modificar o valor 
+
+function changeImage() {
+    const img = document.getElementById("myImage");
+    //aqui conseguimos armazenar o conteudo da imagem 
+
+    const currentSrc = img.getAttribute("src");
+    //aqui pegamos o tributo SRC da imagem, responsavel pela alteração da img.
+
+
+    if (currentSrc === "imagem01.png" ) {//se (if)a origem atual da imagem for igual a "imagem01.png"
+        img.setAttribute("src", "imagem02.png");
+        //usando a setAttribute vamos modificar o valor da src, especificanddi a outra imagem.
+        img.setAttribute("alt","imagem desenho animado verde");
+    
+    } else {//se não, fazer o processo inverso basicamente 
+    img.setAttribute("src", "imagem01.png");
+    img.setAttribute("alt", "imagem desenho animado preto");    
+    }
+
+}
+
+// if = se acondição for valida execute a algo
+// else = caso contrario / se não 
---
+++ aula11.js
@@ -0,0 +1,11 @@
+//DECLARAÇÃO DE UMA FUNÇÃO
+
+console.log('Ola, boa noite');
+console.log('Pegue aqui a sua comanda');
+console.log('Bom apetite');
+
+function pegarComanda () {
+    console.log ( 'Olá, boa noite! ');
+    console.log('Pegue aqui a sua comanda');
+    console.log('Bom apetite');
+}
