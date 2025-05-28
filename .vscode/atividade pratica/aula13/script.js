c7b517434d20f1f9a439b50c5fd756a94104c33d
welton2425 <welton.ma.tu@gmail.com>
2025-05-27 21:45:25 -0300

    javaescript

---
+++ .vscode/atividade pratica/aula13/script.js
@@ -0,0 +1,9 @@
+//aqui estamos recuperando nosso ID que esta no HTML
+let meuElemento = document.getElementById("meuElemento");
+
+function tornarCirculo() {
+    meuElemento.classList.add("circle");// adiciono uma class para arredondar o quadrado
+    meuElemento.classList.remove("");// removo uma class para arredondar a caixa
+    }
+
+    function tornar
