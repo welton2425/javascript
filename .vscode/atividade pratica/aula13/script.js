56dfe2efc79ec046fdf8a85e501160d202cf6cb6
welton2425 <welton.ma.tu@gmail.com>
2025-05-29 21:36:08 -0300

    javascript
    javascript

--- .vscode/atividade pratica/aula13/script.js
+++ .vscode/atividade pratica/aula13/script.js
@@ -3,7 +3,23 @@ let meuElemento = document.getElementById("meuElemento");
 
 function tornarCirculo() {
     meuElemento.classList.add("circle");// adiciono uma class para arredondar o quadrado
-    meuElemento.classList.remove("");// removo uma class para arredondar a caixa
+    meuElemento.classList.remove("square");// removo uma class para arredondar a caixa
     }
 
-    function tornar
+    function tornarQuadrado(){
+        meuElemento.classList.add ("square");
+        meuElemento.classList.remove("circle");
+    }
+
+
+function altenarFormato(){
+    meuElemento.classList.toggle("circle");
+    meuElemento.classList.toggle("square");
+}
+
+//toggle é respomsavel por verificar o elemento atual (funciona como se fosse um vigia)
+
+
+
+
+
