6648b925d5cf4d5f44ebc3903acd21ee2e02e2e5
welton2425 <welton.ma.tu@gmail.com>
2025-06-10 20:20:18 -0300

    javascript

---
+++ .vscode/atividade pratica/aula15/script.js
@@ -0,0 +1,18 @@
+
+const options = {
+    strings: [
+        "Bem vindo ao Obra Social Dom Bosco",
+        "Desenvolvimento Front-end",
+        "Desevolvimento Back-end",
+        "Cadastre-se aqui"
+    ],
+
+    typeSpeed: 60,
+    backSpeed: 40,
+    loop: true,
+    backDelay: 1500,
+    smartBackspace: true 
+};
+
+
+new Typed("#qualquercoisa", options);
